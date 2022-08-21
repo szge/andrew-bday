d204e58d69aeb242f21ea008e08dbb1dd510239d
Jonathan Chiang <jchiangconan@gmail.com>
2022-08-21 17:57:57 -0400

    added room navigation system

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,9 @@ const inventory = new Set<Items>();
 
 function initializeRooms(): void {
     const room1 = new Rooms.Room1();
+    const room2 = new Rooms.Room2();
+    room1.east = room2;
+    room2.west = room1;
     currentRoom = room1;
 }
 
@@ -84,17 +87,8 @@ async function echo(chatCtl: ChatController): Promise<void> {
             self: false,
             avatar: "-",
         });
-    } else if (text.value.startsWith("go to ")) {
-
-    } else if (text.value.startsWith("go to")) {
-        await chatCtl.addMessage({
-            type: "text",
-            content: `Correct usage: go to <room>`,
-            self: false,
-            avatar: "-",
-        });
     } else if (text.value.startsWith("go ")) {
-
+        goTo(chatCtl, text.value.slice(3));
     } else if (text.value.startsWith("go")) {
         await chatCtl.addMessage({
             type: "text",
@@ -156,6 +150,101 @@ async function pickUp(chatCtl: ChatController, item_name: string): Promise<void>
     });
 }
 
+async function goTo(chatCtl: ChatController, direction: string): Promise<void> {
+    const direction_sane = direction.trim().toLowerCase();
+    if (["north", "east", "south", "west"].includes(direction_sane)) {
+        if ((direction_sane === "north" && currentRoom.north != null)
+        || (direction_sane === "east" && currentRoom.east != null)
+        || (direction_sane === "south" && currentRoom.south != null)
+        || (direction_sane === "west" && currentRoom.west != null)) {
+            switch (direction_sane) {
+                case "north": {
+                    await chatCtl.addMessage({
+                        type: "text",
+                        content: `You walk to the room to the north.`,
+                        self: false,
+                        avatar: "-",
+                    });
+                    if (currentRoom.north) currentRoom = currentRoom.north;
+                    break;
+                }
+                case "east": {
+                    await chatCtl.addMessage({
+                        type: "text",
+                        content: `You walk to the room to the east.`,
+                        self: false,
+                        avatar: "-",
+                    });
+                    if (currentRoom.east) currentRoom = currentRoom.east;
+                    break;
+                }
+                case "south": {
+                    await chatCtl.addMessage({
+                        type: "text",
+                        content: `You walk to the room to the south.`,
+                        self: false,
+                        avatar: "-",
+                    });
+                    if (currentRoom.south) currentRoom = currentRoom.south;
+                    break;
+                }
+                case "west": {
+                    await chatCtl.addMessage({
+                        type: "text",
+                        content: `You walk to the room to the west.`,
+                        self: false,
+                        avatar: "-",
+                    });
+                    if (currentRoom.west) currentRoom = currentRoom.west;
+                    break;
+                }
+            }
+        } else {
+            const valid_directions: string[] = [];
+            if (currentRoom.north != null) valid_directions.push("north");
+            if (currentRoom.east != null) valid_directions.push("east");
+            if (currentRoom.south != null) valid_directions.push("south");
+            if (currentRoom.west != null) valid_directions.push("west");
+            let message = `You cannot go ${direction_sane} from this room. Try going `;
+            switch (valid_directions.length) {
+                case 0: {
+                    console.log("wtf. the current room has no connected rooms!!");
+                    break;
+                }
+                case 1: {
+                    message += valid_directions[0] + ".";
+                    break;
+                }
+                case 2: {
+                    message += valid_directions[0] + " or " + valid_directions[1] + ".";
+                    break;
+                }
+                case 3: {
+                    message += valid_directions[0] + ", " + valid_directions[1] + ", or " + valid_directions[2];
+                    break;
+                }
+                case 4: {
+                    message += valid_directions[0] + ", " + valid_directions[1] + ", " + valid_directions[2] + ", or " + valid_directions[3];
+                    break;
+                }
+            }
+            await chatCtl.addMessage({
+                type: "text",
+                content: message,
+                self: false,
+                avatar: "-",
+            });
+        }
+    } else {
+        await chatCtl.addMessage({
+            type: "text",
+            content: `${direction_sane} is not a valid direction. Try "north", "east", "south", or "west".`,
+            self: false,
+            avatar: "-",
+        });
+    }
+}
+
 async function restartGame(chatCtl: ChatController): Promise<void> {
     initializeRooms();
     await chatCtl.addMessage({
@@ -174,7 +263,6 @@ async function helpPrompt(chatCtl: ChatController): Promise<void> {
         content: `"look": Describes surroundings.
 "pick up <item>": Picks up item, if allowed.
 "go <direction>": Moves that direction, if allowed.
-"go to <room>": Go back to a room you have visited.
 "inventory": Tells you what you're holding.
 "drop <item>": Drops item on floor.
 "restart": Restarts game from beginning.
--- src/room.ts
+++ src/room.ts
@@ -1,17 +1,28 @@
 import { Items } from "./App";
-
+/*
+Room layout looks like this:
+ 1 | 2 | 3
+===========
+ 4 | 5 | 6
+===========
+   | 7 | 8
+ ^ (empty room)
+*/
 export abstract class Room {
-    up: Room | null;
-    right: Room | null;
-    down: Room | null;
-    left: Room | null;
+    north: Room | null;
+    east: Room | null;
+    south: Room | null;
+    west: Room | null;
 
     constructor() {
-        this.up = null;
-        this.right = null;
-        this.down = null;
-        this.left = null;
+        this.north = null;
+        this.east = null;
+        this.south = null;
+        this.west = null;
     }
+
+    // talk to whatever in the room
+    abstract talk(): string;
 
     // interact with the room in some way
     abstract use(action: string, currentInventory: Set<Items>): void;
@@ -30,6 +41,14 @@ export class Room1 extends Room {
         this.lighterOnGround = true;
     }
 
+    talk() {
+        if (this.dogIsFed) {
+            return `You talk to the dog.\n*chewing noises*`;
+        } else {
+            return `You talk to the dog.\n*barking noises*`
+        }
+    }
+
     use(action: string, currentInventory: Set<Items>) {
 
     }
@@ -37,12 +56,28 @@ export class Room1 extends Room {
     description() {
         if (this.dogIsFed) {
             if (this.lighterOnGround) {
-                return `You see the letter "D" scrawled on the wall. The Dog is happily chewing away at the meat you gave it. There is a golden lighter on the ground.`;
+                return `You see the letter "D" scrawled on the wall. There are open doorways to the south and east. The Dog is happily chewing away at the meat you gave it. There is a golden lighter on the ground.`;
             } else {
-                return `You see the letter "D" scrawled on the wall. The Dog is happily chewing away at the meat you gave it.`;
+                return `You see the letter "D" scrawled on the wall. There are open doorways to the south and east. The Dog is happily chewing away at the meat you gave it.`;
             }
         } else {
-            return `You see the letter "D" scrawled on the wall. An angry, hungry Dog sits in the corner of the room. It looks like it is guarding a shiny object.`;
+            return `You see the letter "D" scrawled on the wall. There are open doorways to the south and east. An angry, hungry Dog sits in the corner of the room. It looks like it is guarding a shiny object.`;
         }
     }
 }
+
+export class Room2 extends Room {
+    talk() {
+        return `I used to operate a big natural gas plant in the day. I don't really know how I ended up in this room. I miss turning those valves.
+Now I just shuffle around in this room all day. You know, when I first got here there was a N painted on the floor that wore away from all my pacing around.`;
+    }
+
+    use(action: string, currentInventory: Set<Items>) {
+
+    }
+
+    description(): string {
+        return `There is a Natural gas plant manager staring out of the window. There are open doorways to the east, south, and west.`;
+    }
+}
+
